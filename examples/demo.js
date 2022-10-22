a49d099179b55a7f20f4e15d186e56422db923db
Wayne_Mac <wayne65@gmail.com>
2022-10-22 18:39:15 +0800

    feat: add FP crawler

--- examples/demo.js
+++ examples/demo.js
@@ -1,11 +1,17 @@
 'use strict';
 const mkt_crawler = require('../index.js');
+const fp_crawler = require('../lib/fp_crawler.js');
 
 (async () => {
 	// *** Initialize ***
 	await mkt_crawler.initialize();
+	await fp_crawler.initialize();
 
-	// *** GetShoesQuantity ***
+	// *** FP GetFloor ***
+	let fp = await fp_crawler.getFloor();
+	console.log(fp);
+
+	// *** MKT GetShoesQuantity ***
 	let aryHits = [];
 	for (let i = 1; i < 5; i++) {
 		aryHits.push(await mkt_crawler.getShoesQuantity({ shoeType: i }));
@@ -18,9 +24,10 @@ const mkt_crawler = require('../index.js');
 		'\nEpic: ', aryHits[3]
 	);
 
-	// *** GetRunnersQuantity ***
+	// *** MKT GetRunnersQuantity ***
 	console.log('\nTotal runners: ' + (await mkt_crawler.getRunnersQuantity()));
 
 	// *** Close ***
 	await mkt_crawler.close();
+	await fp_crawler.close();
 })();
--- server.js
+++ server.js
@@ -3,22 +3,32 @@ const { JsonDB, Config } = require('node-json-db');
 const fmlog = require('@waynechang65/fml-consolelog').log;
 const basic_f = require('./lib/basic_f.js');
 const mkt_crawler = require('./index.js');
+const fp_crawler = require('./lib/fp_crawler.js');
 
-let db;
+let db1;
+let db2;
 
 async function init() {
+	await fp_crawler.initialize().then(() => {
+		fmlog('sys_msg', ['INIT', 'fp_crawler initialized.']);
+	});
+
 	await mkt_crawler.initialize().then(() => {
 		fmlog('sys_msg', ['INIT', 'mkt_crawler initialized.']);
 	});
-	db = new JsonDB(new Config('./db/stepnDB.json', true, false, '/'));
+	
+	db1 = new JsonDB(new Config('./db/stepnDB.json', true, false, '/'));
+	db2 = new JsonDB(new Config('./db/stepnDB2.json', true, false, '/'));
 }
 
 async function timerGetData() {
 	// Debug的時候，縮短週期時間的一個除數(正常時候是 1, 測試的時候可以設 3，加速)
-	let debug_shortenDurationMultiplier = 1;
+	let debug_shortenDurationMultiplier = 10;
 	try {
 		setInterval(async () => {
 			let intRunners = await mkt_crawler.getRunnersQuantity();
+			let fpFloor = await fp_crawler.getFloor();
+			
 			let aryShoes = [];
 			for (let i = 1; i < 5; i++) {
 				aryShoes.push(
@@ -37,16 +47,32 @@ async function timerGetData() {
 				basic_f.getCurrentDateTime(),
 			]);
 
+			fmlog('crawler_msg', [
+				'Refresh',
+				'FP',
+				`shoes_com: ${fpFloor.shoes.com}, scrolls_com: ${fpFloor.scrolls.com}, rb1: ${fpFloor.gems.rb1}, e1: ${fpFloor.gems.e1}`,
+				'',
+				basic_f.getCurrentDateTime(),
+			]);
+
 			let timeNow = Date.now();
-			await db.push(
+			await db1.push(
 				'/stepn/',
 				[{ time: timeNow, runners: intRunners, shoes: aryShoes }],
 				false
 			);
+
+			await db2.push(
+				'/stepn/',
+				[{ time: timeNow, floor: fpFloor }],
+				false
+			);
+
 		}, (1000 * 60 * 5) / debug_shortenDurationMultiplier); // 5分鐘
 	} catch (err) {
 		console.error(err);
 		await mkt_crawler.close();
+		await fp_crawler.close();
 	}
 }
 
